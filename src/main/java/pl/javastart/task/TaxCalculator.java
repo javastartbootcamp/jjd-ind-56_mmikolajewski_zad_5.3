6e235b53dfa1590a1885152b0020baaa9665ef99
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2022-12-10 14:01:33 +0000

    Rozwiązanie 2.1 poprawione
    usunięte getery i setery oraz niepotrzebna zmienna

--- src/main/java/pl/javastart/task/TaxCalculator.java
+++ src/main/java/pl/javastart/task/TaxCalculator.java
@@ -1,11 +1,10 @@
 package pl.javastart.task;
 
 public class TaxCalculator {
-    Product product;
-    private double foodVat = 0.08;
-    private double fuelVat = 0.23;
-    private double alcoholVat = 0.33;
-    private double standardVat = 0.25;
+    private final double foodVat = 0.08;
+    private final double fuelVat = 0.23;
+    private final double alcoholVat = 0.33;
+    private final double standardVat = 0.25;
 
     private double getBruttoPrice(Product product) {
         return switch (product.getCategory()) {
@@ -18,37 +17,5 @@ public class TaxCalculator {
 
     void showBruttoPrice(Product product) {
         System.out.println("Cena brutto: " + getBruttoPrice(product) + " zł.");
-    }
-
-    public double getFoodVat() {
-        return foodVat;
-    }
-
-    public void setFoodVat(double foodVat) {
-        this.foodVat = foodVat;
-    }
-
-    public double getFuelVat() {
-        return fuelVat;
-    }
-
-    public void setFuelVat(double fuelVat) {
-        this.fuelVat = fuelVat;
-    }
-
-    public double getAlcoholVat() {
-        return alcoholVat;
-    }
-
-    public void setAlcoholVat(double alcoholVat) {
-        this.alcoholVat = alcoholVat;
-    }
-
-    public double getStandardVat() {
-        return standardVat;
-    }
-
-    public void setStandardVat(double standardVat) {
-        this.standardVat = standardVat;
     }
 }
