4033d8cfb051665b3873a6eaf87f46c58e6c57fc
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2022-12-10 01:44:42 +0000

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Product czekolada = new Product("Czekolada Mleczna", "chwila rozkoszy, smutki rozproszy",
                 3.0, "food");
-        Product benzyna = new Product(" DIESEL", "ziemska podróż, kosmiczna cena", 9.0, "fuel");
+        Product benzyna = new Product(" Diesel", "ziemska podróż, kosmiczna cena", 9.0, "fuel");
         Product wodka = new Product("Wesoła 0.1L", "statystyczna małpeczka", 5, "alcohol");
         Product ciasteczko = new Product("kremóweczka 100g", "siódme niebo", 17.1);
 
